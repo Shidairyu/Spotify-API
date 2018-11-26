161314074138e0a725615ec368818c882c4850a8
Dario Berther <shidairyu@gmail.com>
2018-11-26 19:31:26 +0100

    Added first functionality tests.

---
+++ SpotifyPlaylistCreator/SpotifyPlaylistCreator/Program.cs
@@ -0,0 +1,24 @@
+﻿using SpotifyAPI.Web;
+using SpotifyAPI.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyPlaylistCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var spotify = new SpotifyWebAPI()
+            {
+                UseAuth = false,
+            };
+
+            var fullTrack = spotify.GetTrack("3Hvu1pq89D4R0lyPBoujSv");
+            Console.Write(fullTrack.Name);
+        }
+    }
+}
